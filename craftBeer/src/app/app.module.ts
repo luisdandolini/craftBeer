31b6ca812fdf677486f24934fca60073e9ace7c3
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-10-24 21:18:46 -0300

    add style components nav and button

--- craftBeer/src/app/app.module.ts
+++ craftBeer/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ButtonComponent } from './layout/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ craftBeer/src/app/layout/button/button.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent implements OnInit {
+
+  textButton = "Login";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
