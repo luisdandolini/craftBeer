c5f4d9f1a981f8092608cfdee90bd2932c2a16b5
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2022-10-26 09:10:12 -0300

    add routing all-products

--- craftBeer/src/app/app-routing.module.ts
+++ craftBeer/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllProductsComponent } from './pages/all-products/all-products.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "products",
+    component: AllProductsComponent
   }
 ];
 
